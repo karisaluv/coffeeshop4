150814a7940170b3d927923890454dace1160952
karisaluv <salnafizh@gmail.com>
2023-09-17 20:55:02 +0700

    mengupload project coffeeshop4

--- views/V_tambah-barang.php
+++ views/V_tambah-barang.php
@@ -1,59 +1,86 @@
-
 <?php
-//modular untuk memanggil file dari folder template
+//session_start();
+//modular memanggil file dari folder tampleate
 include_once 'template/header.php';
 include_once 'template/sidebar.php';
 include_once 'template/topbar.php';
 ?>
+<body class="bg-gradient-primary">
 
-<div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Tambah Barang</h1>
-                            </div>
-                            <form action="routers/r_barang.php?aksi=tambah" method="POST" class="user" enctype="multiple/form-data">
+<div class="container">
 
-
-                                <!-- ini form email yang menerima inputan email dari user  -->
-                                <div class="form-group">
-                                    <input type="number" class="form-control form-control-user" id="nama_barang"
-                                        placeholder="Nama Barang" name="nama_barang">
-                                </div>
-
-                                <!-- ini form password yang menerima inputan password dari user  -->
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="stock"
-                                        placeholder="Stock" name="stock">
-                                </div>
-
-                                <!-- ini form role yang menerima inputan role dari user  -->
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="harga"
-                                        placeholder="Harga" name="harga">
-                                </div>
-
-                                <!-- <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                    Register Account
-                                </a> -->
-
-                                <div class="input-field">
-                                    <input type="submit" class="btn btn-primary btn-user btn-block" value="Tambah" id="tambah" name="tambah">
-                                </div>
-
-                                <hr>
-                            </form>
-                            <hr>
+    <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Bikin Akun Yok!</h1>
                         </div>
+
+                        <form action="../routers/R_barang.php?aksi=tambah" method="POST" class="user" enctype = "multipart/form-data">
+
+                            <!--untuk menampung inputan id user -->
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user"
+                                    placeholder="Id" name="id" hidden>
+                            </div>
+                            <!--untuk menampung nama dari user-->
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user"
+                                    placeholder="Nama Barang" name="nama">
+                            </div>
+                            <!--untuk menampung email dari user-->
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="exampleInputEmail"
+                                    placeholder="Quantity" name="qty">
+                            </div>
+
+                            <!--untuk menampung password dari user-->
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="password"
+                                    placeholder="Harga" name="harga">
+                            </div>
+
+                            <!--untuk menampung nama dari user-->
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="role"
+                                    value="user" name="role" hidden>
+                            </div>
+                           
+                            <div class="input-field">
+                                <input type="file" value="Choose file" id="register" name="register">
+                            </div>
+
+                            <div class="input-field">
+                                <input type="submit" class="btn btn-primary btn-user btn-block" value="Tambahkan" id="register" name="register">
+                            </div>
+
+                        </form>
+                        
+                
+                       
                     </div>
                 </div>
             </div>
         </div>
- 
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="../assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
 <?php
-//modular untuk memanggil file dari folder template
-include_once 'template/footer.php'; 
+    include_once 'template/footer.php';
 ?>
